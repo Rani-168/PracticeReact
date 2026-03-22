577480facce3fc2aabdb81020f979b64f1ffb951
Rani <ranibhosale168@gmail.com>
2026-03-22 22:14:17 +0530

    react new file added

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import ProductSection from "./component/ProductSection";
 function App() {
   const newArrivals = [
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img1.png",
       title: "T-shirt with Tape Details",
       price: 120,
       rating: 4.5
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img2.png",
       title: "Skinny Fit Jeans",
       price: 240,
       oldPrice: 260,
@@ -24,13 +24,13 @@ function App() {
       discount: "-20%"
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img3.png",
       title: "Checkered Shirt",
       price: 180,
       rating: 4.5
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img4.png",
       title: "Sleeve Striped T-shirt",
       price: 130,
       oldPrice: 160,
@@ -41,7 +41,7 @@ function App() {
 
   const topSelling = [
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img5.png",
       title: "Vertical Striped Shirt",
       price: 212,
       oldPrice: 232,
@@ -49,19 +49,19 @@ function App() {
       discount: "-20%"
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img6.png",
       title: "Courage Graphic T-shirt",
       price: 145,
       rating: 4
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img7.png",
       title: "Loose Fit Bermuda Shorts",
       price: 80,
       rating: 3
     },
     {
-      img: "https://via.placeholder.com/200",
+      img: "src/assets/img8.png",
       title: "Faded Skinny Jeans",
       price: 210,
       rating: 4.5
